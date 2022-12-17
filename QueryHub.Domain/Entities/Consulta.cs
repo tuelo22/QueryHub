fdadb85b45ae4baf0190aed3097c77c4d6d02014
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2022-12-17 14:06:28 -0300

    Ajsutando consuta

--- QueryHub.Domain/Entities/Consulta.cs
+++ QueryHub.Domain/Entities/Consulta.cs
@@ -7,15 +7,13 @@ namespace QueryHub.Domain.Entities
     public class Consulta : IValidation 
     {
         public Guid Identificador { get; private set; } 
-        public Fornecedor Fornecedor { get; private set; }
         public Credencial Credencial { get; private set; }
         public Contrato Contrato { get; set; }
         public PlanoDeTarifacao PlanoDeTarifacao { get; set; }
 
-        public Consulta(Fornecedor fornecedor, Credencial credencial, Contrato contrato, PlanoDeTarifacao planoDeTarifacao)
+        public Consulta(Credencial credencial, Contrato contrato, PlanoDeTarifacao planoDeTarifacao)
         {
             Identificador = Guid.NewGuid();
-            Fornecedor = fornecedor;
             Credencial = credencial;
             Contrato = contrato;
             PlanoDeTarifacao = planoDeTarifacao;
@@ -25,11 +23,6 @@ namespace QueryHub.Domain.Entities
 
         public void Validation()
         {
-            if (Fornecedor == null)
-            {
-                throw new ArgumentNullException("Fornecedor");
-            }
-
             if (Credencial == null)
             {
                 throw new ArgumentNullException("Credencial");
